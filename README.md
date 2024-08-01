aaa8b1e581fd5e601914ccaa42f59872c047a456
JorgeArturo1892 <j.arturo1892@gmail.com>
2024-07-31 20:40:07 -0600

    update repository

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Project-repository
 Proyecto Sprint 6
+Nuestro nuevo proyecto ahonda en la creación de esta sencilla, pero interactiva y divertida aplicación web para
+profundizar en la visualización de los datos que tenemos acerca de los vehículos que tenemos en venta para cualquier
+afortunado que quisiera hacer la comparación de cada uno de los automóviles y camionetas que están a un click de
+que sean tuyos. Lo único que debes hacer es dar click en la casilla de verificación en nuestros gráficos que nos muestran 
+la información del odómetro con respecto a las millas/kilómetros recorridos y el precio, de acuerdo a la marca y tipo de vehículo.
+Pudimos apoyarnos de nuestra CLI para la creación del entorno virtual en el que se trabajó el proyecto, descargando las librerías
+necesarias para su desarrollo, el uso de streamlit, creación de gráficos del tipo histograma y de dispersión para nuestra
+visualización de datos, generamos los archivos necesarios para la creación de la aplicación en Render.
--- app.py
+++ app.py
@@ -2,6 +2,33 @@ import pandas as pd
 import plotly_express as px
 import streamlit as st
 
-cars_df = pd.read_csv('vehicles_us.csv')
-fig = px.histogram(cars_df, x="odometer")  # crear un histograma
-fig.show()  # crear gráfico de dispersión
+cars_df = pd.read_csv('vehicles_us.csv')  # leer los datos de los vehículos
+# creación de una casilla de verifiación
+hist_check = st.checkbox('Construir histograma')
+
+if hist_check:  # hacer click en la casilla
+    # encabezado
+    st.header('Consulta de datos de vehículos')
+    # escribir un mensaje
+    st.write('Creación de un histograma para nuestro conjunto de datos de anuncios de venta de vehículos con base en la columna odómetro')
+
+    # creación de un gráfico de tipo histograma
+    fig = px.histogram(cars_df, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+# creación de una segunda casilla de verificación
+disp_check = st.checkbox('Construir gráfico de dispersión')
+
+if disp_check:  # hacer click en la segunda casilla
+    # encabezado
+    st.header('Nueva visualización de datos')
+    # escribir nuevo mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos del anuncio de venta de vehículos')
+
+    # creación del gráfico
+    # creación de un gráfico de dispersión
+    fig = px.scatter(cars_df, x="odometer", y="price")
+    # crear gráfico de dispersión
+    st.plotly_chart(fig, use_container_width=True)
